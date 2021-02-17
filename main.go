f764183be4985119f9e329de46ca01f4def1dd1e
Christian Gärtner <chrisdagardner@gmail.com>
2021-02-17 15:12:25 +0100

    add option to generate full report and allow csv export

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,7 +15,11 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
+var fullReport = flag.Bool("full-report", false, "generate a full report where each dependency is listed with their respective license")
+var format = flag.String("format", "table", "output format, either 'table' (default) or 'csv'")
+
 func main() {
+	flag.Parse()
 	err := run(context.Background(), os.Args)
 	if err != nil {
 		log.Fatal(err)
@@ -22,27 +27,29 @@ func main() {
 }
 
 func run(ctx context.Context, args []string) error {
-	pathToLockFile := args[1]
+	pathToLockFile := args[len(args)-1]
 
 	lockFileContent, err := ioutil.ReadFile(pathToLockFile)
 	if err != nil {
 		return err
 	}
 
-	packages, err := parsePackages(lockFileContent)
+	lockFile, err := parseLockFile(lockFileContent)
 	if err != nil {
 		return err
 	}
 
-	licenses := map[License]int{}
+	licenseCount := map[License]int{}
+	licenses := map[string]License{}
 	l := sync.Mutex{}
 
-	bar := pb.StartNew(len(packages))
+	bar := pb.StartNew(len(lockFile.Packages))
+	bar.SetWriter(os.Stderr)
 
 	sem := semaphore.NewWeighted(4)
 	wg := sync.WaitGroup{}
 
-	for _, name := range packages {
+	for _, p := range lockFile.Packages {
 		wg.Add(1)
 		err := sem.Acquire(ctx, 1)
 		if err != nil {
@@ -56,25 +63,49 @@ func run(ctx context.Context, args []string) error {
 			}
 
 			l.Lock()
-			licenses[license] = licenses[license] + 1
+			licenses[name] = license
+			licenseCount[license] = licenseCount[license] + 1
 			l.Unlock()
 
 			bar.Increment()
 			sem.Release(1)
 			wg.Done()
-		}(name)
+		}(p.Description.Name)
 	}
 	wg.Wait()
 	bar.Finish()
 
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"License", "# Found"})
-	table.SetFooter([]string{"Total", fmt.Sprintf("%d", len(packages))})
-	for l, c := range licenses {
-		table.Append([]string{string(l), fmt.Sprintf("%d", c)})
-	}
+	if *format == "table" {
+		if *fullReport {
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"Dependency", "Version", "License"})
+			for name, l := range licenses {
+				table.Append([]string{name, lockFile.Packages[name].Version, string(l)})
+			}
+			table.Render()
+		} else {
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"License", "# Found"})
+			table.SetFooter([]string{"Total", fmt.Sprintf("%d", len(licenseCount))})
+			for l, c := range licenseCount {
+				table.Append([]string{string(l), fmt.Sprintf("%d", c)})
+			}
 
-	table.Render()
+			table.Render()
+		}
+	} else if *format == "csv" {
+		if *fullReport {
+			fmt.Println("Dependency,Version,License")
+			for name, l := range licenses {
+				fmt.Printf("%s,%s,%s\n", name, lockFile.Packages[name].Version, string(l))
+			}
+		} else {
+			fmt.Println("License, # Found")
+			for l, c := range licenseCount {
+				fmt.Printf("%s,%d\n", string(l), c)
+			}
+		}
+	}
 
 	return nil
 }
--- parse.go
+++ parse.go
@@ -2,20 +2,61 @@ package main
 
 import "gopkg.in/yaml.v3"
 
-type lockFile struct {
-	Packages map[string]interface{}
+type parse struct {
+	Packages map[string]struct {
+		Dependency  string
+		Description interface{}
+		Source      string
+		Version     string
+	}
 }
 
-func parsePackages(lockFileContent []byte) ([]string, error) {
-	l := lockFile{}
-	err := yaml.Unmarshal(lockFileContent, &l)
+type LockFile struct {
+	Packages map[string]Package
+}
+
+type PackageDescription struct {
+	Name string
+	URL  string
+}
+
+type Package struct {
+	Dependency  string
+	Description PackageDescription
+	Source      string
+	Version     string
+}
+
+func parseLockFile(lockFileContent []byte) (*LockFile, error) {
+	internal := parse{}
+	err := yaml.Unmarshal(lockFileContent, &internal)
 	if err != nil {
 		return nil, err
 	}
 
-	var packageNames []string
-	for name, _ := range l.Packages {
-		packageNames = append(packageNames, name)
+	l := LockFile{Packages: map[string]Package{}}
+
+	for k, v := range internal.Packages {
+		var descr PackageDescription
+		if m, ok := v.Description.(map[string]interface{}); ok {
+			descr = PackageDescription{
+				Name: m["name"].(string),
+				URL:  m["url"].(string),
+			}
+		} else if s, ok := v.Description.(string); ok {
+			descr = PackageDescription{
+				Name: s,
+				URL:  "local",
+			}
+		}
+
+		l.Packages[k] = Package{
+			Dependency:  v.Dependency,
+			Description: descr,
+			Source:      v.Source,
+			Version:     v.Version,
+		}
 	}
-	return packageNames, nil
+
+	return &l, nil
 }
